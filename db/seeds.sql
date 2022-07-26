0d2588e368b0a8c2ba518d917123048b7d40a0e0
lshillman <lshillman@users.noreply.github.com>
2022-07-26 12:23:37 -0700

    create database and seed with starter records

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO movies (movie_name)
+VALUES ("Starship Troopers"),
+       ("Robocop"),
+       ("Predator"),
+       ("Cloud Atlas"),
+       ("Matrix");
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, "Action-packed commentary on the future of government in the intergalactic space age!"),
+       (2, "Humanity prevails over machines and evildoers!"),
+       (3, "A predator predates people."),
+       (4, "It's what Crash (2005) intended to be, but actually good!"),
+       (5, "Great movie; too bad it's been coopted by the incels"),
+       (1, "Makes you want to jump out of a perfectly good spaceship!"),
+       (4, "Six genres in one movie, with Hugo Weaving. Need I say more?");
